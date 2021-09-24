4783153c2530988c7fef8c8368c83c52fb781414
l-proger <l-proger@yandex.ru>
2021-09-24 03:09:37 +0300

    Build ANTLR with visual studio 2022

--- ResolveDependencies.ps1
+++ ResolveDependencies.ps1
@@ -1,7 +1,8 @@
-$zipFileLocalPath = "$PSScriptRoot/antlr4-cpp-runtime-4.9.1-source.zip"
+$zipFileLocalPath = "$PSScriptRoot/antlr-source.zip"
+$rootSrcDirectory = "$PSScriptRoot/antlr_src"
 
 if(-not [System.IO.File]::Exists($zipFileLocalPath)){
-    wget https://www.antlr.org/download/antlr4-cpp-runtime-4.9.1-source.zip -outfile $zipFileLocalPath
+    wget https://www.antlr.org/download/antlr4-cpp-runtime-4.9.2-source.zip -outfile $zipFileLocalPath
 }
 
 
@@ -22,7 +23,7 @@ function CopyDirectory() {
 }
 
 
-$rootSrcDirectory = "$PSScriptRoot\antlr_src"
+
 Remove-Item $rootSrcDirectory -Recurse -ErrorAction Ignore
 Expand-Archive -LiteralPath $zipFileLocalPath -DestinationPath "$rootSrcDirectory"
 
@@ -34,9 +35,11 @@ Write-Host "Copying header files ..."
 
 CopyDirectory -sourceDir "$rootSrcDirectory\runtime\src" -targetDir "$rootDeployDirectory\runtime" -include "*.h"
 
-$vsEdition = "Community"
 
-$vsPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\$vsEdition\"
+$vsVersion = 2022
+$vsEdition = "Preview"
+
+$vsPath = "C:\Program Files\Microsoft Visual Studio\$vsVersion\$vsEdition\"
 
 if ($vsPath -and (Test-Path "$vsPath\Common7\Tools\vsdevcmd.bat")) {
     $json = $(& "${env:COMSPEC}" /s /c "`"$vsPath\Common7\Tools\vsdevcmd.bat`" -no_logo -arch=x64 && powershell -Command `"Get-ChildItem env: | Select-Object Key,Value | ConvertTo-Json`"")
@@ -51,14 +54,19 @@ if ($vsPath -and (Test-Path "$vsPath\Common7\Tools\vsdevcmd.bat")) {
     }
 }
 
-
-pushd "$rootSrcDirectory/runtime"
-msbuild antlr4cpp-vs2019.vcxproj /p:configuration="Release Static" /p:platform=x64
-msbuild antlr4cpp-vs2019.vcxproj /p:configuration="Debug Static" /p:platform=x64
+mkdir "$rootSrcDirectory/build"
+pushd "$rootSrcDirectory/build"
+cmake ../
+    pushd "$rootSrcDirectory/build/runtime"
+    msbuild antlr4_static.vcxproj  /p:configuration="Release" /p:platform=x64
+    msbuild antlr4_static.vcxproj  /p:configuration="Debug" /p:platform=x64
+    popd
 popd
 
-CopyDirectory -sourceDir "$rootSrcDirectory\runtime\bin" -targetDir "$rootDeployDirectory\lib" -include "*.dll"
-CopyDirectory -sourceDir "$rootSrcDirectory\runtime\bin" -targetDir "$rootDeployDirectory\lib" -include "*.lib"
+
+CopyDirectory -sourceDir "$rootSrcDirectory\dist\Debug" -targetDir "$rootDeployDirectory\lib\Debug" -include "*.lib"
+CopyDirectory -sourceDir "$rootSrcDirectory\dist\Debug" -targetDir "$rootDeployDirectory\lib\Debug" -include "*.pdb"
+CopyDirectory -sourceDir "$rootSrcDirectory\dist\Release" -targetDir "$rootDeployDirectory\lib\Release" -include "*.lib"
 
 Remove-Item $rootSrcDirectory -Recurse -ErrorAction Ignore
 Remove-Item $zipFileLocalPath -Recurse -ErrorAction Ignore
