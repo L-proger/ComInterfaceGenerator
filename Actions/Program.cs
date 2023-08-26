b0fa750c07b894c8c134370f5247ac99dc9e6d74
L-proger <l-proger@yandex.ru>
2023-08-26 04:35:25 +0300

    *

---
+++ Actions/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+using Actions.Library;
+using System.Runtime.CompilerServices;
+
+public static class Application {
+    
+    static string CurrentFileDir([CallerFilePath]string filePath = null) {
+        return System.IO.Path.GetDirectoryName(filePath);
+    }
+
+    static string GetProjectDir() {
+        return System.IO.Path.GetDirectoryName(CurrentFileDir());
+    }
+
+
+    public static int Main() {
+
+        var projectDir = GetProjectDir();
+
+        var buildDir = TempDirectory.New();
+        CMake.Configure(projectDir, buildDir);
+        CMake.Build(buildDir);
+
+        var tmpDir = Actions.Library.TempDirectory.New();
+
+
+        return 0;
+    }
+}
